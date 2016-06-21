2d4ff9c8c8518e32f472ce4751f057b270de5c29
rajeev4k <rajeevkumar@Rajeevs-MBP.home>
2016-06-21 16:36:35 -0400

    Latest Commit

--- Gather1.R
+++
@@ -1,3 +0,0 @@
-sitegdp="https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
-download.file(sitegdp, destfile = "./Analysis/Data/getdata%2Fdata%2FGDP.csv", method="curl")
-
--- Gather2.R
+++
@@ -1,2 +0,0 @@
-siteed="https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
-download.file(siteed, destfile = "./Analysis/Data/getdata%2Fdata%2FEDSTATS_Country.csv", method="curl")
